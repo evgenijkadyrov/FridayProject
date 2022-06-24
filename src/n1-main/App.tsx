64ec3e0d2dc624ed857d5153c355ed3d9ead1022
Yauheni Kadyrau <7077634@gmail.com>
2022-06-24 12:30:49 +0300

    without reducer

--- src/n1-main/App.tsx
+++ src/n1-main/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import Login from "../n2-features/f1-auth/a1-login/Login";
 import Tests from "../n2-features/f0-test/Tests";
 import Profile from "./m1-ui/Profile/Profile";
@@ -14,23 +14,18 @@ function App() {
     return (
         <div className="App">
 
-           {/* <Header/>
+            {/* <Header/>
             <Main/>*/}
             <HashRouter>
-            <Routes>
-                <Route path={'/profile'}
-                       element={<Profile/>}/>
-
-                <Route path='/tests'
-
-                       element={<Tests/>}/>
-                <Route path={'login/recover/newpassword'}
-                       element={<NewPassword/>}/>
-                <Route path={'login/recover'} element={<RecoverPassword/>}/>
-                <Route path={'/*'} element={<Ups/>}/>
-                <Route path={'login/registration'} element={<Registration/>}/>
-                <Route path={'/login'} element={<Login/>}/>
-            </Routes>
+                <Routes>
+                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/tests'} element={<Tests/>}/>
+                    <Route path={'login/recover/newpassword'} element={<NewPassword/>}/>
+                    <Route path={'login/recover'} element={<RecoverPassword/>}/>
+                    <Route path={'/*'} element={<Ups/>}/>
+                    <Route path={'login/registration'} element={<Registration/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                </Routes>
             </HashRouter>
         </div>
     );
